5393b60067d1fc69f3e0969cd0661fe9729409eb
lcy0x1 <arthurlcy0x1@gmail.com>
2019-06-30 18:44:10 -0700

    add more stuff in Entity

--- src/entity/Engine.java
+++ src/entity/Engine.java
@@ -1,5 +1,6 @@
 package entity;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
@@ -8,30 +9,35 @@ public class Engine {
 
 	public int time;
 
-	private final Map<Integer,List<Entity>> entities = new TreeMap<>();
-	
-	public void update(int t) {
-		handleCollision();
+	private final Map<Integer, List<Entity>> entities = new TreeMap<>();
+
+	public void add(Entity e) {
+		int key = e.atk << 16 | e.base;
+		List<Entity> l;
+		if (entities.containsKey(key))
+			l = entities.get(key);
+		else
+			entities.put(key, l = new ArrayList<Entity>());
+		l.add(e);
 	}
-	
-	private void handleCollision() {
-		entities.forEach((i,l)->{
-			int atk=i>>16&65535;
-			if(atk==0)
+
+	public void update(int t) {
+		entities.forEach((i, l) -> l.forEach(e -> e.update(t)));
+		entities.forEach((i, l) -> {
+			int atk = i >> 16 & 65535;
+			if (atk == 0)
 				return;
-			entities.forEach((j,q)->{
-				int base=j&65535;
-				if((atk&base)==0)
+			entities.forEach((j, q) -> {
+				int base = j & 65535;
+				if ((atk & base) == 0)
 					return;
-				for(Entity e0:l)
-					for(Entity e1:q)
+				for (Entity e0 : l)
+					for (Entity e1 : q)
 						e0.collide(e1);
 			});
 		});
-	}
-	
-	public void add(Entity e) {
-		
+		entities.forEach((i, l) -> l.forEach(e -> e.post()));
+		entities.forEach((i, l) -> l.removeIf(e -> e.isDead()));
 	}
 
 }
--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -1,17 +1,23 @@
 package entity;
 
 public abstract class Entity {
-	
-	public static final int C_ENEMY=1,C_PLAYER=2,C_BULLET=4,C_PATK=8;
-	
-	protected final int base,atk;
-	
-	public Entity(int i0,int i1) {
-		base=i0;
-		atk=i1;
+
+	public static final int C_ENEMY = 1, C_PLAYER = 2, C_BULLET = 4, C_PATK = 8;
+
+	protected final int base, atk;
+
+	public Entity(int i0, int i1) {
+		base = i0;
+		atk = i1;
 	}
 
 	/** this entity attacks the entity e */
 	protected abstract void collide(Entity e);
-	
+
+	protected abstract boolean isDead();
+
+	protected abstract void post();
+
+	protected abstract void update(int t);
+
 }
